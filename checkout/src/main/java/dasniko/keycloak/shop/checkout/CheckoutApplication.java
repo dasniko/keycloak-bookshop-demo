0e320b77501e3ad80aa35de170e79e5476684d7e
Niko Köbler <niko@n-k.de>
2021-05-22 16:49:29 +0200

    clean up

--- checkout/src/main/java/dasniko/keycloak/shop/checkout/CheckoutApplication.java
+++ checkout/src/main/java/dasniko/keycloak/shop/checkout/CheckoutApplication.java
@@ -2,20 +2,12 @@ package dasniko.keycloak.shop.checkout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @SpringBootApplication
 public class CheckoutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CheckoutApplication.class, args);
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
-        return new ServletListenerRegistrationBean<>(new HttpSessionEventPublisher());
     }
 
 }
