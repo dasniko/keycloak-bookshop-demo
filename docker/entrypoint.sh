bd294708dcfa31762d8b58650b2a72e1a0303c72
Niko Köbler <niko@n-k.de>
2023-06-06 23:58:45 +0200

    wait-for-it

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "Waiting for Keycloak to start"
-until $(curl --output /dev/null --silent --head --fail http://keycloak:8080); do
-    printf '.'
-    sleep 2
-done
-
 echo "Starting apps..."
 QUARKUS_JAVA_OPTS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager"
 java -jar /deployments/pim.jar $QUARKUS_JAVA_OPTS &
