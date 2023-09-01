28ec7ff335b5a67c6758c4f6ff789eee9b3c31ce
Niko Köbler <niko@n-k.de>
2023-09-01 17:06:08 +0200

    remove spring-based checkout

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,8 +5,6 @@ QUARKUS_JAVA_OPTS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.j
 java -jar /deployments/pim.jar $QUARKUS_JAVA_OPTS &
 java -jar /deployments/cart.jar $QUARKUS_JAVA_OPTS &
 java -jar /deployments/shop.jar $QUARKUS_JAVA_OPTS &
-java -jar /deployments/checkout.jar &
-java -jar /deployments/msgbroker.jar &
 
 wait -n
 exit $?
