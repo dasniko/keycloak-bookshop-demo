c082cea1db34ad02c55e6de171353b31e1b314a8
Niko Köbler <niko@n-k.de>
2023-12-21 11:21:52 +0100

    Update project run configurations and upgrade application versions
    This commit updates the run configurations for "pim" and "shop", including changes to MavenSettings. The artifactId and version of modules in pom.xml files are also updated. New security property is added to shop's application.properties file. Changes in Docker files reflect the updated versions and the execution of cart service in entrypoint.sh file is slightly adjusted. The overall modifications aim at improving the application setup and running procedures.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mvn clean package
-docker build -t dasniko/bookshop -f docker/Dockerfile .
-#docker buildx build --pull --platform linux/amd64,linux/arm64 -t dasniko/bookshop -f docker/Dockerfile . --push
+docker build -t dasniko/bookshop:0.0.3 -f docker/Dockerfile .
+#docker buildx build --pull --platform linux/amd64,linux/arm64 -t dasniko/bookshop:0.0.3 -f docker/Dockerfile . --push
 docker image prune -f
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Starting apps..."
 QUARKUS_JAVA_OPTS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager"
 java -jar /deployments/pim.jar $QUARKUS_JAVA_OPTS &
-java -jar /deployments/cart.jar $QUARKUS_JAVA_OPTS &
 java -jar /deployments/shop.jar $QUARKUS_JAVA_OPTS &
+java -jar /deployments/cart.jar &
 nginx -g 'daemon off;' &
 
 wait -n
