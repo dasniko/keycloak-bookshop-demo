e371270c47cae6d172a481a6762a06da9d62a8a4
Niko Köbler <niko@n-k.de>
2024-06-19 16:41:47 +0200

    java 21, quarkus 3.11, ubi9

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IMAGE=dasniko/bookshop
-TAG=0.0.6
+TAG=0.0.7
 mvn clean package
 docker build -t ${IMAGE}:${TAG} -f docker/Dockerfile .
 #docker buildx build --pull --platform linux/amd64,linux/arm64 -t ${IMAGE}:${TAG} -f docker/Dockerfile . --push
