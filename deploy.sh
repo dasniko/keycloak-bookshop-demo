4944b8246fd9d190cbedab80bf6768bb8b414ab6
Niko Köbler <niko@n-k.de>
2024-08-18 19:22:36 +0200

    release w/ bootstrap and updated deps

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IMAGE=dasniko/bookshop
-TAG=0.0.7
+TAG=0.0.8
 mvn clean package
-docker build -t ${IMAGE}:${TAG} -f docker/Dockerfile .
+docker build --pull -t ${IMAGE}:${TAG} -f docker/Dockerfile .
 #docker buildx build --pull --platform linux/amd64,linux/arm64 -t ${IMAGE}:${TAG} -f docker/Dockerfile . --push
 docker image prune -f
