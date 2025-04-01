ab0c213ec77e3117e5e7ac7ddc2033944da7b054
Niko Köbler <niko@n-k.de>
2025-04-01 22:33:48 +0200

    react on signals

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,13 @@
 
 echo "Starting apps..."
 QUARKUS_JAVA_OPTS="-Dquarkus.http.host=0.0.0.0 -Djava.util.logging.manager=org.jboss.logmanager.LogManager"
-java -jar /deployments/pim.jar $QUARKUS_JAVA_OPTS &
-java -jar /deployments/shop.jar $QUARKUS_JAVA_OPTS &
+java -jar /deployments/pim.jar "$QUARKUS_JAVA_OPTS" &
+java -jar /deployments/shop.jar "$QUARKUS_JAVA_OPTS" &
 java -jar /deployments/cart.jar &
 nginx -g 'daemon off;' &
+
+trap "exit" SIGINT
+trap "exit" SIGTERM
 
 wait -n
 exit $?
