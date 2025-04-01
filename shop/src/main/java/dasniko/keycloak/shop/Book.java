b5ed06eb77d87b493d89e6114b576a6ce542efed
Niko Köbler <niko@n-k.de>
2025-04-01 22:33:16 +0200

    use records instead of pojos

--- shop/src/main/java/dasniko/keycloak/shop/Book.java
+++ shop/src/main/java/dasniko/keycloak/shop/Book.java
@@ -1,28 +1,4 @@
 package dasniko.keycloak.shop;
 
-/**
- * @author Niko Köbler, https://www.n-k.de, @dasniko
- */
-public class Book {
-    int id;
-    String title;
-    String author;
-
-    public Book(int id, String title, String author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
+public record Book(int id, String title, String author) {
 }
