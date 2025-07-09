6db667933a637ef48993ad2c2d4e9eae43290731
Niko Köbler <niko@n-k.de>
2025-07-09 18:31:23 +0200

    Enable SSL/TLS across services and configure secure HTTPS communication.

--- checkout/vite.config.js
+++ checkout/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'node:fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 	return {
 		plugins: [react()],
 		server: {
+			https: {
+				cert: fs.readFileSync('.tls/localhost.crt'),
+				key: fs.readFileSync('.tls/localhost.key'),
+			},
 			open: true,
 			port: 3000,
+			strictPort: true,
 		}
 	};
 });
