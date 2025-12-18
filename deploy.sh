4ea1fe4b366b078f23404b7101ca66f323299079
Niko Köbler <niko@n-k.de>
2025-12-18 22:24:00 +0100

    bump version to 0.0.10 and update related references

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IMAGE=dasniko/bookshop
-TAG=0.0.9
+TAG=0.0.10
 mvn clean package
 docker build --pull -t ${IMAGE}:${TAG} -f docker/Dockerfile .
 #docker buildx build --pull --platform linux/amd64,linux/arm64 -t ${IMAGE}:${TAG} -f docker/Dockerfile . --push
