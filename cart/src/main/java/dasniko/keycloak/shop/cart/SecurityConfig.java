87d6c4c2dfc53eb4151b0166b5aad4c80d7033bf
Niko Köbler <niko@n-k.de>
2026-01-09 12:52:04 +0100

    Support multiple signature algorithms dynamically

--- cart/src/main/java/dasniko/keycloak/shop/cart/SecurityConfig.java
+++ cart/src/main/java/dasniko/keycloak/shop/cart/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtValidators;
@@ -51,7 +52,23 @@ public class SecurityConfig {
 			@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri,
 			@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuerUri,
 			@Value("${spring.security.oauth2.resourceserver.jwt.audience}") String audience) {
-		NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+		NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(jwkSetUri)
+				.jwsAlgorithms(algorithms -> {
+					// Support multiple signature algorithms dynamically
+					// RS256, RS384, RS512 (RSA with SHA-256, SHA-384, SHA-512)
+					algorithms.add(SignatureAlgorithm.RS256);
+					algorithms.add(SignatureAlgorithm.RS384);
+					algorithms.add(SignatureAlgorithm.RS512);
+					// PS256, PS384, PS512 (RSA-PSS with SHA-256, SHA-384, SHA-512)
+					algorithms.add(SignatureAlgorithm.PS256);
+					algorithms.add(SignatureAlgorithm.PS384);
+					algorithms.add(SignatureAlgorithm.PS512);
+					// ES256, ES384, ES512 (ECDSA with SHA-256, SHA-384, SHA-512)
+					algorithms.add(SignatureAlgorithm.ES256);
+					algorithms.add(SignatureAlgorithm.ES384);
+					algorithms.add(SignatureAlgorithm.ES512);
+				})
+				.build();
 
 		OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
 		OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
