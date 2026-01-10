d24ce08a84fe9b2d6efae75eca3cd449de98061c
Niko Köbler <niko@n-k.de>
2026-01-10 10:50:55 +0100

    bump version to 0.0.11

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IMAGE=ghcr.io/dasniko/keycloak-bookshop-demo
-TAG=0.0.10
+TAG=0.0.11
 mvn clean package
 docker build --pull -t ${IMAGE}:${TAG} -f docker/Dockerfile .
 #docker buildx build --pull --platform linux/amd64,linux/arm64 -t ${IMAGE}:${TAG} -f docker/Dockerfile . --push
